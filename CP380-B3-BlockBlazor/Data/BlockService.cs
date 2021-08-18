f982693b4bbde27d117fd06a37d26c512e4576f3
Venkata Penugonda <penugondapavan01@gmail.com>
2021-08-18 12:06:05 -0400

    fisrt commit

--- CP380-B3-BlockBlazor/Data/BlockService.cs
+++ CP380-B3-BlockBlazor/Data/BlockService.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using System.Text.Json;
 using System.Net.Http;
 using Microsoft.Extensions.Configuration;
+using CP380_B1_BlockList.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CP380_B3_BlockBlazor.Data
 {
@@ -14,16 +16,33 @@ namespace CP380_B3_BlockBlazor.Data
         //       - httpClient
         //       - configuration
         //
+        static HttpClient clientFactory;
+        public IConfiguration configuration;
 
         //
         // TODO: Add a constructor with IConfiguration and IHttpClientFactory arguments
         //
+        public BlockService() { }
+        public BlockService(IConfiguration c, IHttpClientFactory http)
+        {
+            clientFactory = http.CreateClient();
+            configuration = c.GetSection("BlockService");
+        }
 
         //
         // TODO: Add an async method that returns an IEnumerable<Block> (list of Blocks)
         //       from the web service
         //
-
+        public async Task<IEnumerable<Block>> ListBlocks()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, configuration["url"]);            
+            var response = await clientFactory.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseStream = await response.Content.ReadAsStreamAsync();
+                return await JsonSerializer.DeserializeAsync<IEnumerable<Block>>(responseStream);
+            }
+            return Array.Empty<Block>();
+        }
     }
-}
 }
--- CP380-B3-BlockBlazor/Data/PendingTransactionService.cs
+++ CP380-B3-BlockBlazor/Data/PendingTransactionService.cs
@@ -5,7 +5,8 @@ using System.Threading.Tasks;
 using System.Text.Json;
 using System.Net.Http;
 using Microsoft.Extensions.Configuration;
-
+using CP380_B1_BlockList.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CP380_B3_BlockBlazor.Data
 {
@@ -15,20 +16,40 @@ namespace CP380_B3_BlockBlazor.Data
         //       - httpClient
         //       - configuration
         //
+        static HttpClient clientFactory;
+        private readonly IConfiguration configuration;
 
         //
         // TODO: Add a constructor with IConfiguration and IHttpClientFactory arguments
-        //
+        //        
+        public PendingTransactionService() { }
+        public PendingTransactionService(IConfiguration c, IHttpClientFactory http)
+        {
+            clientFactory = http.CreateClient();
+            configuration = c.GetSection("PayloadService");
+        }
 
         //
         // TODO: Add an async method that returns an IEnumerable<Payload> (list of Payloads)
         //       from the web service
         //
+        public async Task<IEnumerable<Payload>> ListPayloads()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, configuration["url"]);
+            var response = await clientFactory.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseStream = await response.Content.ReadAsStreamAsync();
+                return await JsonSerializer.DeserializeAsync<IEnumerable<Payload>>(responseStream);
+            }
+            return Array.Empty<Payload>();
+        }
 
         //
         // TODO: Add an async method that returns an HttpResponseMessage
         //       and accepts a Payload object.
         //       This method should POST the Payload to the web API server
         //
+
     }
 }
